9656c3109fbdd03ad3959b9508ca5444cc6b5040
uytyks <uytyks@udel.edu>
2023-12-15 15:38:50 -0500

    excercise5 fully copied over

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://exercise5-3496e-default-rtdb.firebaseio.com"
 });
-
+// Get a database reference to our blog
+const { getDatabase } = require('firebase-admin/database');
+const db = getDatabase();
+const ref = db.ref('server/saving-data/');
+const usersRef = ref.child('users');
+usersRef.set({
+alanisawesome: {
+date_of_birth: 'June 23, 1912',
+full_name: 'Alan Turing'
+},
+gracehop: {
+date_of_birth: 'December 9, 1906',
+full_name: 'Grace Hopper'
+}
+});
 const http = require('http')
 const port = 8080
 // Create a server object:
